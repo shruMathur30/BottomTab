a5f7cb53450005442ac20a77e38e0b8d611056de
Rishi Mathur <rishimathur2004@gmail.com>
2025-07-03 21:09:48 +0530

    Final code submission

--- src/api/dummyTabsAPI.ts
+++ src/api/dummyTabsAPI.ts
@@ -7,45 +7,41 @@ export interface TabItem {
 }
 
 export const fetchTabsFromAPI = (): Promise<TabItem[]> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve([
-        {
-          name: 'Home',
-          componentKey: 'Home',
-          icon: 'home-outline',
-          index: 0,
-          visible: true,
-        },
-        {
-          name: 'Search',
-          componentKey: 'Search',
-          icon: 'search-outline',
-          index: 1,
-          visible: true,
-        },
-        {
-          name: 'Offers',
-          componentKey: 'Offers',
-          icon: 'pricetags-outline',
-          index: 2,
-          visible: false,
-        },
-        {
-          name: 'Notifications',
-          componentKey: 'Notifications',
-          icon: 'notifications-outline',
-          index: 3,
-          visible: true,
-        },
-        {
-          name: 'Profile',
-          componentKey: 'Profile',
-          icon: 'person-outline',
-          index: 4,
-          visible: true,
-        },
-      ]);
-    }, 1000);
-  });
+  return Promise.resolve([
+    {
+      name: 'Home',
+      componentKey: 'Home',
+      icon: 'home-outline',
+      index: 0,
+      visible: true,
+    },
+    {
+      name: 'Search',
+      componentKey: 'Search',
+      icon: 'search-outline',
+      index: 1,
+      visible: true,
+    },
+    {
+      name: 'Offers',
+      componentKey: 'Offers',
+      icon: 'pricetags-outline',
+      index: 2,
+      visible: false,
+    },
+    {
+      name: 'Notifications',
+      componentKey: 'Notifications',
+      icon: 'notifications-outline',
+      index: 3,
+      visible: true,
+    },
+    {
+      name: 'Profile',
+      componentKey: 'Profile',
+      icon: 'person-outline',
+      index: 4,
+      visible: true,
+    },
+  ]);
 };
